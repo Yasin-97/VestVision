6c1adeb749590c527e8c16781621a5edb30497f9
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-04-26 21:54:53 +0330

    configured vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,21 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  define: {
-    "process.env": {},
+  optimizeDeps: {
+    // 👈 optimizedeps
+    esbuildOptions: {
+      target: "esnext",
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      supported: {
+        bigint: true,
+      },
+    },
+  },
+
+  build: {
+    target: ["esnext"], // 👈 build.target
   },
 });
