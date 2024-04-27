8284610614eb3f29a07512f7e0af0ef1996a893f
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-04-27 17:32:31 +0330

    change config of css to default

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  optimizeDeps: {
-    // 👈 optimizedeps
-    esbuildOptions: {
-      target: "esnext",
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
-      supported: {
-        bigint: true,
-      },
-    },
-  },
-
-  build: {
-    target: ["esnext"], // 👈 build.target
+  plugins: [react()],
+  define: {
+    global: "globalThis",
+    "process.env": {},
   },
 });
