dd4d99b566f2b3e28ad88f611163c888d007be90
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-05-01 15:30:19 +0330

    created the Home page UI added related new components

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Sidebar } from "./Sidebar";
 export { default as Button } from "./Button";
 export { default as Loader } from "./Loader";
 export { default as FormField } from "./FormField";
+export { default as DisplayCampaigns } from "./DisplayCampaigns";
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-type Props = {};
+import { DisplayCampaigns } from "../components";
+import { CampaignType, useStateContext } from "../context";
 
-const Home = (props: Props) => {
-  return <div>Home</div>;
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState<CampaignType[]>([]);
+
+  const { address, contract, getCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
 };
 
 export default Home;
