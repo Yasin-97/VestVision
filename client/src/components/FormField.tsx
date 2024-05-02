26fd11ecfac91b7710177e6ea5b952f55fadb125
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-05-02 14:20:34 +0330

    added two new props to FormFiled

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -3,14 +3,18 @@ import { ChangeEvent, ChangeEventHandler } from "react";
 
 type FormFieldType = {
   labelName: string;
+  className: string;
+  textAreaRow: number;
   placeholder: string;
-  value: string | BigNumber;
+  value?: string | BigNumber;
   handleChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
   inputType?: string;
   isTextArea?: boolean;
 };
 const FormField = ({
   labelName,
+  className,
+  textAreaRow = 10,
   placeholder,
   inputType,
   isTextArea,
@@ -29,9 +33,9 @@ const FormField = ({
           required
           value={value}
           onChange={handleChange}
-          rows={10}
+          rows={textAreaRow}
           placeholder={placeholder}
-          className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
+          className={`py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px] ${className}`}
         />
       ) : (
         <input
@@ -41,7 +45,7 @@ const FormField = ({
           type={inputType}
           step="0.1"
           placeholder={placeholder}
-          className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
+          className={`py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px] ${className}`}
         />
       )}
     </label>
