704f46155e2c019f894269321571ee8e88b471a9
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-05-24 16:24:12 +0330

    added loading state to Button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,20 +1,29 @@
 import { ButtonHTMLAttributes } from "react";
+import { RiLoader4Fill } from "react-icons/ri";
 
 type ButtonType = {
   btnType?: "submit" | "reset" | "button";
   title: string;
   handleClick?: () => void;
   styles: string;
+  isLoading?: boolean;
 };
 
-const Button = ({ btnType, title, handleClick, styles }: ButtonType) => {
+const Button = ({
+  btnType,
+  title,
+  handleClick,
+  styles,
+  isLoading,
+}: ButtonType) => {
   return (
     <button
       type={btnType}
-      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+      className={`flex justify-center items-center bg-[#8c6dfd] font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
       onClick={handleClick}
     >
-      {title}
+      {isLoading && <RiLoader4Fill className={`rotate-constant w-8 h-8`} />}
+      {!isLoading && title}
     </button>
   );
 };
