6d8179927e42a5e728bd291b0319c306f8b2dfa9
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-05-24 15:11:09 +0330

    created campaignToken contract

---
+++ web3/contracts/CampaignToken.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ITokenData.sol";
+
+contract CampaignToken is ERC20, ITokenData {
+    address public _campaignOwner;
+
+    string private _name;
+    string private _symbol;
+    uint256 private _totalSupply;
+    uint256 private _campaignOwnerShare;
+    address private _teamAddress;
+    uint256 private _teamShare;
+    address private _advisorAddress;
+    uint256 private _advisorShare;
+    address private _earlyInvestorsAddress;
+    uint256 private _earlyInvestorsShare;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 totalSupply,
+        uint256 campaignOwnerShare,
+        address teamAddress,
+        uint256 teamShare,
+        address advisorAddress,
+        uint256 advisorShare,
+        address earlyInvestorsAddress,
+        uint256 earlyInvestorsShare
+    ) ERC20(name, symbol) {
+        require(
+            campaignOwnerShare +
+                teamShare +
+                advisorShare +
+                earlyInvestorsShare <=
+                100,
+            "Total share exceeds 100%"
+        );
+
+        // Set the campaign owner
+        _campaignOwner = msg.sender;
+        _name = name;
+        _symbol = symbol;
+        _totalSupply = totalSupply;
+        _campaignOwnerShare = campaignOwnerShare;
+        _teamAddress = teamAddress;
+        _teamShare = teamShare;
+        _advisorAddress = advisorAddress;
+        _advisorShare = advisorShare;
+        _earlyInvestorsAddress = earlyInvestorsAddress;
+        _earlyInvestorsShare = earlyInvestorsShare;
+
+        // Calculate tokens for each party
+        uint256 ownerTokens = (totalSupply * campaignOwnerShare) / 100;
+        uint256 teamTokens = (totalSupply * teamShare) / 100;
+        uint256 advisorTokens = (totalSupply * advisorShare) / 100;
+        uint256 reserveTokens = (totalSupply * earlyInvestorsShare) / 100;
+
+        // Calculate public tokens by subtracting allocated tokens from initial supply
+        uint256 publicTokens = totalSupply -
+            (ownerTokens + teamTokens + advisorTokens + reserveTokens);
+
+        // Mint tokens for each party
+        _mint(_campaignOwner, ownerTokens);
+        if (teamAddress != address(0) && teamShare > 0) {
+            _mint(teamAddress, teamTokens);
+        }
+        if (advisorAddress != address(0) && advisorShare > 0) {
+            _mint(advisorAddress, advisorTokens);
+        }
+        if (earlyInvestorsAddress != address(0) && earlyInvestorsShare > 0) {
+            _mint(earlyInvestorsAddress, reserveTokens);
+        }
+        // Mint the rest of the tokens to the contract for the public
+        _mint(address(this), publicTokens);
+    }
+
+    function tokenData()
+        external
+        view
+        override
+        returns (
+            string memory,
+            string memory,
+            uint256,
+            address,
+            uint256,
+            address,
+            uint256,
+            address,
+            uint256,
+            address,
+            uint256
+        )
+    {
+        return (
+            _name,
+            _symbol,
+            _totalSupply,
+            _campaignOwner,
+            _campaignOwnerShare,
+            _teamAddress,
+            _teamShare,
+            _advisorAddress,
+            _advisorShare,
+            _earlyInvestorsAddress,
+            _earlyInvestorsShare
+        );
+    }
+}
