59f50c2ff4b4d48ad48b7764fed0aa85ede0a1d9
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-06-05 09:16:18 +0330

    fix(toolbar): fixed error of auto selecting first option on toolbar on focus
    changed button to span tag to fix the issue

--- client/src/components/dashboard/Toolbar.tsx
+++ client/src/components/dashboard/Toolbar.tsx
@@ -1,4 +1,5 @@
 import { LuHeading1, LuHeading2, LuHeading3 } from "react-icons/lu";
+
 import { LuItalic } from "react-icons/lu";
 import { LuBold } from "react-icons/lu";
 import { Editor } from "@tiptap/react";
@@ -7,25 +8,23 @@ type ToolbarProps = { editor: Editor };
 export const Toolbar = ({ editor }: ToolbarProps) => {
   return (
     <div className="flex gap-2 p-2 border-b border-[#3a3a43]">
-      <button
+      <span
         onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
         className={editor.isActive("bold") ? "bg-slate-700 rounded-[4px]" : ""}
       >
         <LuBold className="text-base text-slate-100 m-1 font-medium" />
-      </button>
+      </span>
 
-      <button
+      <span
         onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
         className={
           editor.isActive("italic") ? "bg-slate-700 rounded-[4px]" : ""
         }
       >
         <LuItalic className="text-base text-slate-100 m-1 font-medium" />
-      </button>
+      </span>
 
-      <button
+      <span
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
         className={
           editor.isActive("heading", { level: 1 })
@@ -34,8 +33,8 @@ export const Toolbar = ({ editor }: ToolbarProps) => {
         }
       >
         <LuHeading1 className="text-base text-slate-100 m-1" />
-      </button>
-      <button
+      </span>
+      <span
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
         className={
           editor.isActive("heading", { level: 2 })
@@ -44,8 +43,8 @@ export const Toolbar = ({ editor }: ToolbarProps) => {
         }
       >
         <LuHeading2 className="text-base text-slate-100 m-1" />
-      </button>
-      <button
+      </span>
+      <span
         onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
         className={
           editor.isActive("heading", { level: 3 })
@@ -54,7 +53,7 @@ export const Toolbar = ({ editor }: ToolbarProps) => {
         }
       >
         <LuHeading3 className="text-base text-slate-100 m-1" />
-      </button>
+      </span>
     </div>
   );
 };
