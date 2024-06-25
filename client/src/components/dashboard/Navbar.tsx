48cebde1d459c39cd34e108d0485fc784a5a30d6
Yasin Zahiri <yasin.zahiri10@gmail.com>
2024-06-25 21:10:44 +0330

    feat(navbar): showed elements of navbar conditionally based on pages

--- client/src/components/dashboard/Navbar.tsx
+++ client/src/components/dashboard/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useStateContext } from "../../context";
 import { Button } from "..";
 import { logo, menu, search } from "../../assets";
@@ -11,39 +11,47 @@ const Navbar = (props: Props) => {
   const { address, connect, handleSearchChange, searchquery } =
     useStateContext();
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const isCreateCampaign = pathname === "/dashboard/create-project";
+  const isProfile = pathname === "/dashboard/profile";
+  const isMainDashboard = pathname === "/dashboard";
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
-      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
-        <input
-          value={searchquery}
-          onChange={(e) => handleSearchChange(e.target.value)}
-          type="text"
-          placeholder="Search for Projects"
-          className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
-        />
+      {(isMainDashboard || isProfile) && (
+        <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+          <input
+            value={searchquery}
+            onChange={(e) => handleSearchChange(e.target.value)}
+            type="text"
+            placeholder="Search for Projects"
+            className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
+          />
 
-        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
-          <img
-            src={search}
-            alt="search"
-            className="w-[15px] h-[15px] object-contain"
+          <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+            <img
+              src={search}
+              alt="search"
+              className="w-[15px] h-[15px] object-contain"
+            />
+          </div>
+        </div>
+      )}
+      {!isCreateCampaign && (
+        <div className="sm:flex hidden flex-row justify-end gap-4">
+          <Button
+            btnType="button"
+            title={address ? "Create a project" : "Connect"}
+            styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+            handleClick={() => {
+              if (address) navigate("/dashboard/create-project");
+              else connect();
+            }}
           />
         </div>
-      </div>
-      <div className="sm:flex hidden flex-row justify-end gap-4">
-        <Button
-          btnType="button"
-          title={address ? "Create a project" : "Connect"}
-          styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
-          handleClick={() => {
-            if (address) navigate("/dashboard/create-project");
-            else connect();
-          }}
-        />
-      </div>
+      )}
       <div className="sm:hidden flex justify-between items-center relative">
         <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
           <img
